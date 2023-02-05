56828dd1ac3d1abbe536e4f8f61b6fc69f01d763
hyjun2352 <khyj030502@gmail.com>
2023-02-05 21:07:19 +0900

    [docs][style] 추가&변경

--- server/apps/posts/forms.py
+++ server/apps/posts/forms.py
@@ -1,8 +1,13 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import User
+from .models import User, Post
 
 class SignupForm(UserCreationForm):
   class Meta:
     model = User
     fields = ['member_id', 'password1', 'password2', 'name', 'age']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'content')
--- server/apps/posts/views.py
+++ server/apps/posts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from server.apps.posts.forms import SignupForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import auth
-from .models import User
+from .models import User, Post, Comment
 
 def main(request):
     return render(request, "posts/main.html")
@@ -48,3 +48,48 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return redirect('posts:main')
+
+from .forms import PostForm
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt #보안 관련 부분
+
+@csrf_exempt
+def like_ajax(request, *args, **kwargs):
+    req = json.loads(request.body)
+
+    post_id = req['id']
+    post = Post.objects.get(id = post_id)
+
+    if post.like == True:
+        post.like = False
+    else:
+        post.like = True
+    post.save()
+
+    liked = post.like
+    return JsonResponse({'id': post_id, 'liked': liked})
+
+@csrf_exempt #403에러 방지
+def comment_ajax(request, *args, **kwargs):
+    req = json.loads(request.body) #{id:1, content: ...}
+
+    post_id = req['id'] #1
+    content = req['content'] #...
+
+    comment = Comment.objects.create(
+        post = Post.objects.get(id=post_id),
+        content = content,
+    )
+
+    return JsonResponse({'post_id': post_id, 'comment_id': comment.id, 'content': comment.content})
+
+@csrf_exempt
+def comment_del_ajax(request, *args, **kwargs):
+    req = json.loads(request.body)
+
+    post_id = req['post_id']
+    comment_id = req['comment_id']
+    comment = Comment.objects.get(id=comment_id)
+    comment.delete()
+    return JsonResponse({'post_id': post_id, 'comment_id': comment_id})
