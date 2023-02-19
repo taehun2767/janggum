7ad0cfd442b0b31cd4e5d28e0d00454d0dd3175e
sunjae <zxsunje775@naver.com>
2023-02-19 19:56:54 +0900

    [fix]settings.py

--- server/config/urls.py
+++ server/config/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -22,7 +23,10 @@ urlpatterns = [
     path('', include('server.apps.posts.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    re_path(r'^static/(?P<path>.*)$',serve,{'document_root':settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
 ]
+
 
 
 if settings.DEBUG:
